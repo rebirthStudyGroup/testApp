27c702955ec0feedd9afe4d6085666213c961093
shogo <shogo@example.com>
2019-03-09 20:07:10 +0900

    My App , firs commit

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class User(models.Model):
+    name=models.CharField(max_length=30)
+    mail_address=models.CharField(max_length=200)
+    password = models.CharField(max_length=8)
+    admin=models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
